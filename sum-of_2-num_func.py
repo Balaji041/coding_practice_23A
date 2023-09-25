8d76437ebe5fcfe1eb2f39af2e1c1f99d9320a1d
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-25 23:24:18 +0530

    Create sum-of_2-num_func.py

---
+++ sum-of_2-num_func.py
@@ -0,0 +1,16 @@
+def add(arg_1, arg_2):
+    # Complete this function
+    return arg_2+arg_1
+
+a = int(input())
+b = int(input())
+# Call the add function
+print(add(a,b))
+
+
+"""
+input:
+2
+3
+output:5
+"""
