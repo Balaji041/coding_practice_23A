190599b28fe9ea6446480a988b12dcd9befac74f
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-25 23:28:09 +0530

    Create printing-message.py

---
+++ printing-message.py
@@ -0,0 +1,15 @@
+def message(arg_1, arg_2):
+    # Complete this function
+    return arg_1+" is "+str(arg_2)+" years old."
+name = input()
+age = int(input())
+# Call the message function
+print(message(name,age))
+
+"""
+inout:
+Akhil
+15
+output:Akhil is 15 years old.
+
+"""
